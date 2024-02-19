392d718917cf6220280a5b38a5449aa5d37ad23b
Vladimir Angirov <angirov@posteo.net>
2024-02-19 17:48:54 +0100

    add request parser

--- Request.cpp
+++ Request.cpp
@@ -1,21 +1,24 @@
 #include "Request.hpp"
 
-std::string Request::process() {
+std::string Request::process()
+{
+    parse();
+    print_request();
     std::string http200chunked = "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Transfer-Encoding: chunked\r\n"
-                        "\r\n";
+                                 "Content-Type: text/plain\r\n"
+                                 "Transfer-Encoding: chunked\r\n"
+                                 "\r\n";
     std::string res;
     res += "RESPONCE: your request was:\n";
     res += "============================\n";
-    res += request;
+    res += request.str();
     res += "============================\n";
     res += "Thank you for using our server!";
 
     std::stringstream ss;
     ss << http200chunked
-        << std::hex << res.length() << "\r\n"
-        << res << "\r\n0\r\n\r\n";
+       << std::hex << res.length() << "\r\n"
+       << res << "\r\n0\r\n\r\n";
 
     return ss.str();
 }
--- Request.hpp
+++ Request.hpp
@@ -1,18 +1,156 @@
 #ifndef REQUEST_HPP
-# define REQUEST_HPP
+#define REQUEST_HPP
 
-# include <string>
-# include <iostream>
-# include <sstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <cctype> // for strip
 
-class Request {
+#include "utils.hpp"
+
+enum Method
+{
+    MethodInvalid,
+    MethodGET
+};
+
+enum HTTPVersion
+{
+    HTTPVerInvalid,
+    HTTPVer10,
+    HTTPVer11
+};
+
+enum StatusCode
+{
+    StatusCodeInvalid,
+    StatusCode200,
+
+};
+
+typedef std::map<std::string, std::vector<std::string> > header_map;
+
+struct Request
+{
+// private:
 public:
-    std::string request;
-    Request(const std::string & request) : request(request){
+    // Server
+    static std::string default_host;
+    // Connetion
+    bool connectionKeepAlive;
+
+    std::istringstream  request;
+
+    Method       method;
+    std::string  url; // should be a class??? query params?
+    HTTPVersion  httpVersion;
+    header_map   headers; // conditions GET (changed since last request ), obligatory host header for virtual hosting, Connection: Keep-Alive default for http11 otherwise "close"
+    std::string  body;
+
+    void parse_first() {
+        std::string first_line;
+        std::getline(request, first_line);
+        std::istringstream iss(first_line);
+        std::string word;
+        iss >> word;
+        method = resolveMethod(word);
+
+        iss >> word;
+        url = word; ////////////////////////////// need a url handler?????????????????????????????????????????????????
+        iss >> word;
+        httpVersion = resolveHTTPVersion(word);
+    }
+    
+
+    void parse_header(std::string line) {
+        std::stringstream lineStream(line);
+        std::string key;
+        std::getline(lineStream, key, ':');
+        // check key?
+
+        std::string value;
+        std::list<std::string> values_l;
+        while (std::getline(lineStream, value, ',')) { // it is a mess: https://stackoverflow.com/questions/29549299
+            values_l.push_back(strip(value));
+        }
+        // while (lineStream >> value) {
+        //     values_l.push_back(strip(value));
+        // }
+        std::vector<std::string> values(values_l.begin(), values_l.end());
+
+        headers.insert(header_map::value_type(strip(key), values)); // the values should not be overwritten -> check if key is there
+    }
+
+    void parse()
+    {
+        parse_first();
+        std::string header_line;
+        while (std::getline(request, header_line) && header_line != "\r") { // is there a beter way to deal with it ????????????????????????????
+                parse_header(header_line);
+        }
+        std::stringstream rest;
+        rest << request.rdbuf();
+        body = rest.str();
+    }
+
+    void print_headers(std::stringstream & ss) {
+        for (header_map::iterator it = headers.begin(); it != headers.end(); ++it) {
+            ss << "Header: " << (*it).first << ";  values: [ " << std::endl;
+            (*it).second; // str vec
+            for (std::vector<std::string>::iterator val_it = (*it).second.begin(); val_it != (*it).second.end(); ++val_it) {
+                ss << "    " << *val_it << std::endl;
+            }
+            ss << "  ]" << std::endl;
+        }
+    }
+
+    void print_request() {
+        std::stringstream ss;
+        ss << "method: " << method << std::endl;
+        ss << "url: " << url << std::endl;
+        ss << "httpVersion: " << httpVersion << std::endl;
+        print_headers(ss);
+        ss << "body: " << body << std::endl;
+        std::cout << ss.str() << std::endl;
 
     }
 
+
+// public:
+    Request(const std::string &request) : request(request)
+    {
+    }
+
     std::string process();
+
+    Method resolveMethod(std::string const & word) {
+        if(word == "GET" ) return MethodGET;
+        return MethodInvalid;
+    };
+
+    HTTPVersion resolveHTTPVersion(std::string const & word) {
+        if(word == "HTTP/1.0" ) return HTTPVer10;
+        if(word == "HTTP/1.1" ) return HTTPVer11;
+        return HTTPVerInvalid;
+    };
+
+};
+
+class Responce
+{
+    HTTPVersion httpVersion;
+    StatusCode statusCode; // with descriptions hardcoded
+    header_map headers;
 };
 
 #endif
+
+// http11:
+// consistent connetions
+// mandatory server headers
+// bettter caching options
+//
