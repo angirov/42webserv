ce7ff43d93e85919a767c62a5f5b8897569c98b0
mwagner <mwagner@students.42wolfsburg.de>
2024-02-20 18:42:42 +0100

    config class with VirtServer and Location class with copy constructor and setter/getters

--- conf/ConfigClass.cpp
+++ conf/ConfigClass.cpp
@@ -1,90 +1,181 @@
 #include "ConfigClass.hpp"
 
+// Config class implementation
 Config::Config() {
-	// Initialize default values
 	_timeout = 0;
 	_maxClients = 0;
-	_port = 0;
+	_client_max_body_size = 0;
 }
 
-// Setter functions
+Config::Config(const Config& other) {
+	_timeout = other._timeout;
+	_maxClients = other._maxClients;
+	_client_max_body_size = other._client_max_body_size;
+}
+
+// Setter and Getter functions for Config class
 void Config::setTimeout(int timeout) {
-	this->_timeout = timeout;
+	_timeout = timeout;
+}
+
+int Config::getTimeout() const {
+	return _timeout;
 }
 
 void Config::setMaxClients(int maxClients) {
-	this->_maxClients = maxClients;
-}
-
-void Config::setPort(int port) {
-	this->_port = port;
-}
-
-void Config::setServerName(const std::string& serverName) {
-	this->_serverName = serverName;
-}
-
-void Config::setErrorPage(const std::string& errorPage) {
-	this->_errorPage = errorPage;
-}
-
-void Config::setLocationRoot(const std::string& locationRoot) {
-	this->_locationRoot = locationRoot;
-}
-
-void Config::setLocationIndex(const std::string& locationIndex) {
-	this->_locationIndex = locationIndex;
-}
-
-void Config::setMethods(const std::vector<std::string>& methods) {
-	this->_methods = methods;
-}
-
-void Config::setCGI(const std::string& cgi) {
-	this->_cgi = cgi;
-}
-
-void Config::setUploadDir(const std::string& uploadDir) {
-	this->_uploadDir = uploadDir;
-}
-
-// Getter functions
-int Config::getTimeout() const {
-	return _timeout;
+	_maxClients = maxClients;
 }
 
 int Config::getMaxClients() const {
 	return _maxClients;
 }
 
-int Config::getPort() const {
+void Config::setClientMaxBodySize(int maxBodySize) {
+	_client_max_body_size = maxBodySize;
+}
+
+int Config::getClientMaxBodySize() const {
+	return _client_max_body_size;
+}
+
+// VirtServer class implementation
+// Constructor
+VirtServer::VirtServer(int port, const std::string &serverName) {
+	_port = port;
+	_serverName = serverName;
+}
+
+VirtServer::VirtServer(const VirtServer& other) {
+	_port = other._port;
+	_serverName = other._serverName;
+	_errorPages = other._errorPages;
+	locations = other.locations;
+}
+
+// Setter and Getter functions for VirtServer class
+void VirtServer::setPort(int port) {
+	_port = port;
+}
+
+int VirtServer::getPort() const {
 	return _port;
 }
 
-std::string Config::getServerName() const {
+void VirtServer::setServerName(const std::string &serverName) {
+	_serverName = serverName;
+}
+
+const std::string& VirtServer::getServerName() const {
 	return _serverName;
 }
 
-std::string Config::getErrorPage() const {
-	return _errorPage;
+void VirtServer::addErrorPage(int errorCode, const std::string &errorPage) {
+	_errorPages[errorCode] = errorPage;
 }
 
-std::string Config::getLocationRoot() const {
+const std::string &VirtServer::getErrorPage(int errorCode) const {
+	std::map<int, std::string>::const_iterator it = _errorPages.find(errorCode);
+	if (it != _errorPages.end()) {
+		return it->second;
+	}
+	static const std::string emptyString = "";
+	return emptyString;
+}
+
+void VirtServer::addLocation(const Location &location) {
+	locations.push_back(location);
+}
+
+const std::vector<Location> &VirtServer::getLocations() const {
+	return locations;
+}
+
+// Location class implementation
+// Constructor
+Location::Location(const std::string &route, const std::string& locationRoot, const std::string &locationIndex) {
+	_locationRoot = locationRoot;
+	_locationIndex = locationIndex;
+	_autoIndex = false;
+	_route = route;
+}
+
+Location::Location(const Location& other) {
+	_returnRedir = other._returnRedir;
+	_route = other._route;
+	_locationRoot = other._locationRoot;
+	_locationIndex = other._locationIndex;
+	_methods = other._methods;
+	_cgiExtension = other._cgiExtension;
+	_uploadDir = other._uploadDir;
+	_autoIndex = other._autoIndex;
+}
+
+// Setter and Getter functions for Location class
+void Location::setLocationRoot(const std::string& locationRoot) {
+	_locationRoot = locationRoot;
+}
+
+const std::string& Location::getLocationRoot() const {
 	return _locationRoot;
 }
 
-std::string Config::getLocationIndex() const {
+void Location::setLocationIndex(const std::string& locationIndex) {
+	_locationIndex = locationIndex;
+}
+
+const std::string& Location::getLocationIndex() const {
 	return _locationIndex;
 }
 
-std::vector<std::string> Config::getMethods() const {
+void Location::setAutoIndex(bool autoIndex) {
+	_autoIndex = autoIndex;
+}
+
+bool Location::getAutoIndex() const {
+	return _autoIndex;
+}
+
+void Location::setRoute(const std::string &route) {
+	_route = route;
+}
+
+const std::string& Location::getRoute() const {
+	return _route;
+}
+
+void Location::addMethod(const std::string &method) {
+	_methods.push_back(method);
+}
+
+const std::vector<std::string> &Location::getMethods() const {
 	return _methods;
 }
 
-std::string Config::getCGI() const {
-	return _cgi;
+void Location::addCGIExtension(const std::string &cgiExtension) {
+	_cgiExtension.push_back(cgiExtension);
 }
 
-std::string Config::getUploadDir() const {
+const std::vector<std::string>& Location::getCGIExtensions() const {
+	return _cgiExtension;
+}
+
+void Location::setUploadDir(const std::string &uploadDir) {
+	_uploadDir = uploadDir;
+}
+
+const std::string& Location::getUploadDir() const {
 	return _uploadDir;
+}
+
+void Location::addReturnRedir(int errorCode, const std::string& redirectUrl) {
+	_returnRedir[errorCode] = redirectUrl;
+}
+
+const std::string &Location::getReturnRedir(int errorCode) const {
+	std::map<int, std::string>::const_iterator it = _returnRedir.find(errorCode);
+	if (it != _returnRedir.end()) {
+		return it->second;
+	}
+	static const std::string emptyString = "";
+	return emptyString;
 }
--- conf/ConfigClass.hpp
+++ conf/ConfigClass.hpp
@@ -5,42 +5,49 @@
 #include <vector>
 #include <map>
 
+class Location;
+class VirtServer;
+
 class Config {
 public:
 	Config();
+	Config(const Config &other); // Copy constructor
 
-	// Setter functions
+	// Setter and Getter functions for Config class
 	void setTimeout(int timeout);
-	void setMaxClients(int maxClients);
-	void setPort(int port);
-	void setServerName(const std::string& serverName);
-	void setErrorPage(const std::string& errorPage);
-	void setLocationRoot(const std::string& locationRoot);
-	void setLocationIndex(const std::string& locationIndex);
-	void setMethods(const std::vector<std::string>& methods);
-	void setCGI(const std::string& cgi);
-	void setUploadDir(const std::string& uploadDir);
-
-	// Getter functions
 	int getTimeout() const;
+
+	void setMaxClients(int maxClients);
 	int getMaxClients() const;
-	int getPort() const;
-	std::string getServerName() const;
-	std::string getErrorPage() const;
-	std::string getLocationRoot() const;
-	std::string getLocationIndex() const;
-	std::vector<std::string> getMethods() const;
-	std::string getCGI() const;
-	std::string getUploadDir() const;
+
+	void setClientMaxBodySize(int maxBodySize);
+	int getClientMaxBodySize() const;
 
 private:
 	int _timeout;
 	int _maxClients;
-	std::vector<VirtServer> _virtServers;
-	int client_max_body_size;
+	int _client_max_body_size;
 };
 
 class VirtServer {
+public:
+	VirtServer(int port, const std::string &serverName);
+	VirtServer(const VirtServer &other); // Copy constructor
+
+	// Setter and Getter functions for VirtServer class
+	void setPort(int port);
+	int getPort() const;
+
+	void setServerName(const std::string &serverName);
+	const std::string & getServerName() const;
+
+	void addErrorPage(int errorCode, const std::string &errorPage);
+	const std::string & getErrorPage(int errorCode) const;
+
+	void addLocation(const Location &location);
+	const std::vector<Location> & getLocations() const;
+
+private:
 	int _port;
 	std::string _serverName;
 	std::map<int, std::string> _errorPages;
@@ -48,7 +55,37 @@ class VirtServer {
 };
 
 class Location {
-	std::map<int, std::string> _returnRedir; 
+public:
+	Location(const std::string &route, const std::string &locationRoot, const std::string &locationIndex);
+	Location(const Location &other); // Copy constructor
+
+	// Setter and Getter functions for Location class
+	void setRoute(const std::string &route);
+	const std::string & getRoute() const;
+
+	void setLocationRoot(const std::string &locationRoot);
+	const std::string & getLocationRoot() const;
+
+	void setLocationIndex(const std::string &locationIndex);
+	const std::string & getLocationIndex() const;
+
+	void setAutoIndex(bool autoIndex);
+	bool getAutoIndex() const;
+
+	void addMethod(const std::string &method);
+	const std::vector<std::string> & getMethods() const;
+
+	void addCGIExtension(const std::string &cgiExtension);
+	const std::vector<std::string> & getCGIExtensions() const;
+
+	void setUploadDir(const std::string &uploadDir);
+	const std::string & getUploadDir() const;
+
+	void addReturnRedir(int errorCode, const std::string &redirectUrl);
+	const std::string & getReturnRedir(int errorCode) const;
+
+private:
+	std::map<int, std::string> _returnRedir;
 	std::string _route;
 	std::string _locationRoot;
 	std::string _locationIndex;
@@ -56,6 +93,6 @@ class Location {
 	std::vector<std::string> _cgiExtension;
 	std::string _uploadDir;
 	bool _autoIndex;
-}
+};
 
 #endif /* CONFIGCLASS_HPP */
