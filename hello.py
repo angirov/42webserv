ea74ca2488f83f73d4d405fbfbd50349dd0df69f
Vladimir Angirov <angirov@posteo.net>
2024-03-12 10:36:23 +0100

    hello python prints env vars

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import os
+
 print("Content-Type: text/html\r\n")
 print("<html> ")
 print("<head>")
@@ -5,5 +7,10 @@ print("<title> Test CGI </title>")
 print("</head>")
 print("<body>")
 print("<center> <h1>Hello from python</h1> </center> ")
+
+for param in os.environ.keys():
+   print (f"{param}: {os.environ[param]} <br>")
+
+
 print("</body>")
 print("</html>")
