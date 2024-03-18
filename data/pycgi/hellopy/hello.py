a1d305d0eb9b72c71d9870ec3f8432affec265ea
Vladimir Angirov <angirov@posteo.net>
2024-03-18 14:58:53 +0100

    add greeting script for hellopy endpoint

--- data/pycgi/hellopy/hello.py
+++ data/pycgi/hellopy/hello.py
@@ -1,15 +1,16 @@
 import os
 import sys
 import time
+from urllib import parse
 
 sys.stderr.write("Python: starting the script.\n")
 
 content = sys.stdin.read()
 if not content:
-   content=""
+    content = "<EMPTY>"
 sys.stderr.write("Python: stdin: " + content + "\n")
 
-print("Content-Type: text/html\r\n\r\n", end='')
+print("Content-Type: text/html\r\n\r\n", end="")
 
 
 print("<html> ")
@@ -22,13 +23,29 @@ print("<center> <h1>Hello from python!</h1> </center> ")
 
 print("<h2>Env Vars:</h2>")
 for param in os.environ.keys():
-   print (f"{param}: {os.environ[param]} <br>")
+    print(f"{param}: {os.environ[param]} <br>")
 
 print("<h2>Body:</h2>")
 print("Content of stdin:")
 print(content)
 
+
+print("<h2>Greetings: </h2>")
+
+method = os.environ.get("REQUEST_METHOD")
+
+if method != "GET":
+    print("Only GET method allowed for this script!", "<br>")
+else:
+    qs = os.environ.get("QUERY_STRING")
+    if qs:
+        print("Query srting: ", qs, "<br>")
+        name = parse.parse_qs(qs)["name"][0]
+        print("Name: ", name, "<br>")
+
+        print("<h1>", end="")
+        print("Hello ", name, end="")
+        print("!!!!!!!!!!!!!!!!!!</h1>")
+
 print("</body>")
 print("</html>")
-
-time.sleep(1)
