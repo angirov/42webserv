bf338d7700a40d259a27ae4c3332160c53f92a97
Maximilian Wagner <mwagner@students.42wolfsburg.de>
2024-03-19 17:36:22 +0100

    configs load dynamically

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 	if (argc == 2) {
 		configFile = argv[1];
 	} else {
-		configFile = "parser/Configs/default.conf"; // Default configuration file path
+		configFile = "/configs/default.conf"; // Default configuration file path
 	}
 
 	Config config; // Create a Config object
